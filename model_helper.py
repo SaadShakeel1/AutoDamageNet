900aa22a9e8d9d20886c0dff977a37390643cfb2
SaadShakeel1 <saadshakeell448@gmail.com>
2025-08-22 01:05:23 +0500

    Update Project

--- model_helper.py
+++ model_helper.py
@@ -1,51 +1,49 @@
 from PIL import Image
 import torch
 from torch import nn
-from torchvision import models,transforms
+from torchvision import models, transforms
+import os
 
 trained_model = None
 class_names = ["Front Breakage", "Front Crushed", "Front Normal", "Rear Breakage", "Rear Crushed", "Rear Normal"]
 
-# Load the pre-trained ResNet model
+# Define model
 class CarClassifierResNet(nn.Module):
     def __init__(self, num_classes, dropout_rate=0.5):
         super().__init__()
         self.model = models.resnet50(weights='DEFAULT')
-        # Freeze all layers except the final fully connected layer
+        # Freeze all layers except final
         for param in self.model.parameters():
             param.requires_grad = False
-            
-        # Unfreeze layer4 and fc layers
         for param in self.model.layer4.parameters():
-            param.requires_grad = True            
-            
-        # Replace the final fully connected layer
+            param.requires_grad = True
+
         self.model.fc = nn.Sequential(
             nn.Dropout(dropout_rate),
             nn.Linear(self.model.fc.in_features, num_classes)
         )
 
     def forward(self, x):
-        x = self.model(x)
-        return x
+        return self.model(x)
+
 
 def predict(image_path):
-    image=Image.open(image_path).convert("RGB")
+    image = Image.open(image_path).convert("RGB")
     transform = transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    image_tensor=transform(image).unsqueeze(0)
+    image_tensor = transform(image).unsqueeze(0)
 
     global trained_model
-
     if trained_model is None:
-        trained_model = CarClassifierResNet(6)
-        trained_model.state_dict(torch.load("model\saved_model.pth"))
+        trained_model = CarClassifierResNet(num_classes=6)
+        model_path = os.path.join("model", "saved_model.pth")
+        trained_model.load_state_dict(torch.load(model_path, map_location="cpu"))
         trained_model.eval()
 
     with torch.no_grad():
         output = trained_model(image_tensor)
-        _,predicted=torch.max(output,1)
+        _, predicted = torch.max(output, 1)
         return class_names[predicted.item()]
